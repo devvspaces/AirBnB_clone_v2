bbcd651befeefbf213d58df65d4aaed0fd0b3ebe
Netrobe Ubuntu <netrobepy@gmail.com>
2023-10-23 05:56:24 +0100

    update

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""
-Execute a Minimal Flask application
+"""Execute a Minimal Flask application
 """
 from flask import Flask
 
@@ -14,6 +13,4 @@ def hello_hbnb():
 
 
 if __name__ == "__main__":
-    """Main block
-    """
     app.run(host="0.0.0.0")
